09c1d04571016e755d6bc51a41a51492b06c697b
Christos Tsagkournis <chrtsago@yahoo.gr>
2017-12-13 02:37:33 +0200

    Enforcing selinux disabled state and adding firewall rules

--- cookbooks/lilith/recipes/default.rb
+++ cookbooks/lilith/recipes/default.rb
@@ -1,3 +1,15 @@
 template '/etc/selinux/config' do
 	source 'selinux_config.erb'
 end
+
+execute 'selinux_disable' do
+	command 'setenforce 0'
+end
+
+template '/etc/firewalld/zones/public.xml' do
+	source 'firewalld_public.xml.erb'
+end
+
+service 'firewalld' do
+	action :restart
+end
