efa79b08d8a648a96d68c287859854e441f15fec
Christos Tsagkournis <chrtsago@yahoo.gr>
2017-12-20 03:08:24 +0200

    Starting with the smoke test suite for the default recipe

--- cookbooks/nothingness/test/smoke/default/default_test.rb
+++ cookbooks/nothingness/test/smoke/default/default_test.rb
@@ -1,3 +1,50 @@
-describe package('httpd') do
-	it { should be_installed }
+# Are all the essential packages installed?
+packages = [
+	'epel-release',
+	'git',
+	'vim',
+	'wget',
+	'yum-utils',
+	'bash-completion',
+	'tree',
+	'net-tools',
+	'nmap',
+	'bind-utils',
+	'telnet',
+	'strace',
+	'colordiff'
+]
+
+packages.each do |package_name|
+	describe package(package_name) do
+		it { should be_installed }
+	end
+end
+
+# Is SELinux disabled?
+
+# Is the timezone set to Athens, Greece?
+
+# Are ports for ssh, http and https opened up?
+ports = [
+	22,
+	80,
+	443
+]
+
+ports.each do |port_number|
+	describe port(port_number) do
+		it { should be_listening }
+	end
+end
+
+# Is the firewalld service running?
+describe service('firewalld') do
+	it { should be_running }
+end
+
+# Is the postfix service stopped and disabled?
+describe service('postfix') do
+	it { should be_disabled }
+	it { should_not be_running }
 end
