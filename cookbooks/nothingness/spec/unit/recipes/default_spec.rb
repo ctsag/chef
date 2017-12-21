053cad44615f3a6761cea587525473821588e100
Christos Tsagkournis <chrtsago@yahoo.gr>
2017-12-22 00:32:23 +0200

    Adding basic unit tests

--- cookbooks/nothingness/spec/unit/recipes/default_spec.rb
+++ cookbooks/nothingness/spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'nothingness::default' do
-	let(:chef_run) do
-		runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-		runner.converge(described_recipe)
-	end
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+    runner.converge(described_recipe)
+  end
 
-	it 'converges successfully' do
-		expect { chef_run }.to_not raise_error
-	end
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
+  end
 end
