ff55ff329d24d48816a4d868a34e86942b8b54eb
Christos Tsagkournis <chrtsago@yahoo.gr>
2017-12-31 14:06:04 +0200

    Adding a script to allow for automation of remote acceptance tests via inspec

---
+++ cookbooks/nothingness/inspec_env.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+readonly COOKBOOK="nothingness"
+readonly TEST="test/integration"
+
+status=0
+
+if [ $# -ne 1 ] ; then
+    echo "No environment provided. Aborting" >&2
+    exit 1
+else
+    environment="${1}"
+fi
+
+fqdn=`knife search "chef_environment:${environment}" 2>&1 | grep 'FQDN:' | tr -d ' ' | cut -d':' -f2`
+recipes=( `knife search "chef_environment:${environment}" 2>&1 | grep 'Recipes:' | tr -d ' ' | cut -d':' -f2- | sed -e "s/${COOKBOOK}:://g" -e "s/^${COOKBOOK},//g" -e 's/,/ /g' -e 's/export-attributes//g'` )
+
+echo "InSpec tests starting for environment ${environment} on host ${fqdn}"
+for recipe in "${recipes[@]}" ; do
+    echo "- Running test ${TEST}/${recipe}/${recipe}_test.rb on host ${fqdn}"
+    inspec exec ${TEST}/${recipe}/${recipe}_test.rb -t ssh://${fqdn} -i ~/.ssh/id_rsa
+    if [ $? -ne 0 ] ; then
+        status=1
+    fi
+done
+
+echo "InSpec tests done. Exit code is ${status}"
+exit ${status}
